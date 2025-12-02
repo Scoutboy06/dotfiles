754b6f8b3582618aefaab6328cfc0907963364fd
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-12-02 12:31:25 +0100

    Script to check if dotfiles have changed

---
+++ check-status.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DOTFILES_DIR="$HOME/p/dotfiles"
+cd "$DOTFILES_DIR" || exit 1
+
+STATUS=$(git status --porcelain)
+
+if [ -n "$STATUS" ]; then
+    notify-send --wait \
+        "Dotfiles need attention" \
+        "$(printf '%s' "$STATUS" | head -n 10)"
+fi
+
