6f722247ad7dae8ac16846d470848de50b814fd7
Elias Wennerlund <elias06wennerlund@gmail.com>
2026-04-04 02:30:40 +0200

    removed neovim config

--- dot_config/nvim/lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
--- Disable some LazyVim default plugins if needed
--- Useful for customization or reducing startup time
-
-return {
-  -- Example: disable a plugin
-  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-}
