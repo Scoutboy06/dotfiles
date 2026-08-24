42b0501fba3361ed12ad3427cd7a551f4c820442
Elias Wennerlund <elias06wennerlund@gmail.com>
2026-08-24 21:10:08 +0200

    Migrate Hyprland appearance config to Lua

---
+++ dot_config/hypr/looknfeel.lua
@@ -0,0 +1,14 @@
+-- Personal appearance overrides loaded after Omarchy's defaults.
+
+hl.config({
+  general = {
+    gaps_in = 4,
+    gaps_out = 8,
+    resize_on_border = true,
+    resize_corner = true,
+  },
+
+  decoration = {
+    rounding = 8,
+  },
+})
